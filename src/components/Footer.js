116622381ac2249e8109d8d61f2540ed522b273b
Munawar Ali <mnrkokhar@gmail.com>
2023-01-12 11:53:52 +0500

    Update Footer for Mobiles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,30 +10,33 @@ import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => {
   return (
-    <Box my="40px" sx={{backgroundColor:'rgba(255, 38, 37, 0.15)', borderRadius:'30%',}} >
+    <Box my="40px" sx={{backgroundColor:'rgba(255, 38, 37, 0.15)', borderRadius:'20%',}} >
       <Stack sx={{alignItems: 'center', px: '40px', pt: '24px'}}>
         <img src={Logo} alt="Logo" width="200px" height="40px" />
-        <Typography variant="h5" my="40px" alignItems="center">Made with Fun By: <span style={{fontSize: '30px'}} >Munawar❤️😊😘</span></Typography>
-        <Stack direction="row" alignItems="center" gap="80px">
+        <Stack sx={{my: {lg:"40px", xs:'20px'}}} alignItems="center">
+          <Typography variant="h5">Made with Fun By: </Typography>
+          <Typography><span style={{fontSize:'30px'}} >Munawar❤️😊😘</span></Typography>
+        </Stack>
+        <Stack direction="row" sx={{gap: { lg:"80px", xs: "15px"}}} alignItems="center" my="10px" pb="10px" >
             <a href='https://www.linkedin.com/in/munawaralidev' style={{textDecoration: 'none', color:'#000', textAlign:"center"}}>
               <LinkedInIcon fontSize="large" />
-              <Typography variant="h5" pb="40px" mt="5px" align='center'>LinkedIn</Typography>
+              <Typography sx={{ display: { lg: 'block', xs: 'none'}}} variant="h5" pb="40px" mt="5px" align='center'>LinkedIn</Typography>
             </a>
             <a href='https://github.com/MunawarAliAraiz/fitness_app' style={{textDecoration: 'none', color:'#000', textAlign:"center"}}>
               <GitHubIcon fontSize="large" />
-              <Typography variant="h5" pb="40px" mt="5px">GitHub</Typography>
+              <Typography sx={{ display: { lg: 'block', xs: 'none'}}} variant="h5" pb="40px" mt="5px">GitHub</Typography>
             </a>
             <a href='mailto:mnrkokhar@gmail.com' style={{textDecoration: 'none', color:'#000', textAlign:"center"}}>
               <EmailIcon fontSize="large" />
-              <Typography variant="h5" pb="40px" mt="5px">Gmail</Typography>
+              <Typography sx={{ display: { lg: 'block', xs: 'none'}}} variant="h5" pb="40px" mt="5px">Gmail</Typography>
             </a>
             <a href='https://twitter.com/AraizMunawar?t=qGefNlwd7AnC7w1_EPC-Mg&s=09' style={{textDecoration: 'none', color:'#000', textAlign:"center"}}>
               <TwitterIcon fontSize="large" />
-              <Typography variant="h5" pb="40px" mt="5px">Twitter</Typography>
+              <Typography sx={{ display: { lg: 'block', xs: 'none'}}} variant="h5" pb="40px" mt="5px">Twitter</Typography>
             </a>
             <a href='https://instagram.com/munawar_araiz?igshid=YzgyMTM2MGM=' style={{textDecoration: 'none', color:'#000', textAlign:"center"}}>
               <InstagramIcon fontSize="large" />
-              <Typography variant="h5" pb="40px" mt="5px">Instagram</Typography>
+              <Typography sx={{ display: { lg: 'block', xs: 'none'}}} variant="h5" pb="40px" mt="5px">Instagram</Typography>
             </a>
         </Stack>
       </Stack>
